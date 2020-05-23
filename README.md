67ab9dbe684b8db969c5dcb53762eab9a032b79f
Sledmine <angelherxd@hotmail.com>
2020-05-23 12:11:31 -0600

    updated README.md, added scripts

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Workarounds
+# Workarounds for Halo Custom Edition using Lua
+
+Different files for fixing/tweaking/restoring Halo Custom Edtion mainly based in Lua scripts
+for Chimera.
+
+**All the files here are release files, you can drag and drop it in your global scripts folder
+and they must work.** 
+
+## Available scripts:
+- **Dynamic Reverb Sound**:
+  Provides reverberation for sounds that does not have it by default like melee, reload, grenade throw.
+- **FP Animation Permutation**:
+  Reimplements first person animation permutation by using the OpenSauce animation label format.
---
+++ reverb.lua
@@ -0,0 +1,53 @@
+------------------------------------------------------------------------------
+-- Reverb
+-- On the fly sound class patch for reverberation sounds
+-- Author: Sledmine
+-- Version: 2.0
+------------------------------------------------------------------------------
+
+clua_version = 2.042
+
+local debugMode = false
+
+local blam = require 'lua-blam'
+
+--- Function to send debug messages to console output
+---@param message string
+---@param color string | "'category'" | "'warning'" | "'error'" | "'success'"
+function dprint(message, color)
+    if (debugMode) then
+        if (color == 'category') then
+            console_out(message, 0.31, 0.631, 0.976)
+        elseif (color == 'warning') then
+            -- TO DO
+            console_out(message)
+        elseif (color == 'error') then
+            -- TO DO
+            console_out(message)
+        elseif (color == 'success') then
+            console_out(message, 0.235, 0.82, 0)
+        else
+            console_out(message)
+        end
+    end
+end
+
+set_callback('map load', 'onMapLoad')
+
+function onMapLoad()
+    local tagsCount = get_tags_count()
+    for i = 0, tagsCount - 1 do
+        local tagPath = get_tag_path(i)
+        if (tagPath) then
+            if (tagPath:find('sound') and tagPath:find('ready') or tagPath:find('reload') or tagPath:find('throwgren')) then
+                dprint('Found reverb sound!')
+                dprint(tagPath, 'success')
+                local tagObject = blam.sound(get_tag(i))
+                dprint(tagObject.class)
+                blam.sound(get_tag(i), {class = 13})
+            end
+        end
+    end
+end
+
+onMapLoad()
