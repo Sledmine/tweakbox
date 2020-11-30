8bb0054c020ece1a3247e74eb966b6a8ac03606b
Sled <angelherxd@hotmail.com>
2020-11-29 20:35:46 -0700

    lua-blam migration, better implementation, added bundle.json

--- src/fp_animation_permutation.lua
+++ src/fp_animation_permutation.lua
@@ -6,25 +6,34 @@
 ------------------------------------------------------------------------------
 clua_version = 2.042
 
+local keepTimer = true
 local debugMode = false
+local permutatorTimer
 
-local blam = require 'lua-blam'
+local blam = require "blam"
+local tagClasses = blam.tagClasses
+local objectClasses = blam.objectClasses
+console_out = blam.consoleOutput
+
+local glue = require "glue"
+local inspect = require "inspect"
 
 --- Function to send debug messages to console output
 ---@param message string
----@param color string | "'category'" | "'warning'" | "'error'" | "'success'"
+---@param color '"string"' | '"category"' | '"warning"' | '"error"' | '"success"'
 function dprint(message, color)
+    if (type(message) ~= "string") then
+        message = inspect(message)
+    end
     if (debugMode) then
-        if (color == 'category') then
+        if (color == "category") then
             console_out(message, 0.31, 0.631, 0.976)
-        elseif (color == 'warning') then
-            -- TO DO
-            console_out(message)
-        elseif (color == 'error') then
-            -- TO DO
-            console_out(message)
-        elseif (color == 'success') then
-            console_out(message, 0.235, 0.82, 0)
+        elseif (color == "warning") then
+            console_out(message, blam.consoleColors.warning)
+        elseif (color == "error") then
+            console_out(message, blam.consoleColors.error)
+        elseif (color == "success") then
+            console_out(message, blam.consoleColors.success)
         else
             console_out(message)
         end
@@ -32,90 +41,101 @@ function dprint(message, color)
 end
 
 -- List of all the weapon permutable animations in current map
-local permutableAnimations = {}
+local availableAnimations = {}
 
-local animationTable = {
-    ['idle'] = 1,
-    ['posing'] = 2,
-    ['fire'] = 3, -- Leaving this like 'fire' because fire-1 was not working for some reason
-    ['moving'] = 4,
-    ['overlays'] = 5,
+local animationClasses = {
+    ["idle"] = 1,
+    ["posing"] = 2,
+    ["fire"] = 3, -- Leaving this like 'fire' because fire-1 was not working for some reason
+    ["moving"] = 4,
+    ["overlays"] = 5,
     -- I think OpenSauce uses flashlight animations somehow
     -- Anyway if these are working is because you are using OpenSauce, so you don't need the script (?)
-    -- ['light-off'] = 6,
-    -- ['light-on'] = 7,
-    ['reload-empty'] = 8,
-    ['reload-full'] = 9,
-    ['overheated'] = 10,
-    ['ready'] = 11,
-    ['put-away'] = 12, -- This is used for vehicles where seats does not allow weapons, thx aLTis94
-    ['overcharged'] = 13,
-    ['melee'] = 14,
-    -- ['fire-2'] = 15, -- As far as I know fire-2 is never used in any weapon
-    ['overcharged-jitter'] = 16, -- No idea what is this
-    ['throw-grenade'] = 17
+    -- ["light-off"] = 6,
+    -- ["light-on"] = 7,
+    ["reload-empty"] = 8,
+    ["reload-full"] = 9,
+    ["overheated"] = 10,
+    ["ready"] = 11,
+    ["put-away"] = 12, -- This is used for vehicles where seats does not allow weapons, thx aLTis94
+    ["overcharged"] = 13,
+    ["melee"] = 14,
+    -- ["fire-2"] = 15, -- As far as I know fire-2 is never used in any weapon
+    ["overcharged-jitter"] = 16, -- No idea what is this
+    ["throw-grenade"] = 17
 }
 
-function onTimer()
-    for animationTagId, animationPermutations in pairs(permutableAnimations) do
-        local weaponModelAnimations = blam.modelAnimations(
-                                          get_tag(animationTagId))
-        if (weaponModelAnimations) then
-            for animationType, animationPermutationsValues in
-                pairs(animationPermutations) do
-                weaponModelAnimations.fpAnimationList[animationTable[animationType]] =
-                    math.random(animationPermutationsValues[1],
-                                animationPermutationsValues[#animationPermutationsValues])
+function OnTimer()
+    if (keepTimer) then
+        for tagIndex, permAnimationClasses in pairs(availableAnimations) do
+            local tempTag = blam.getTag(tagIndex)
+            local animationsTag = blam.modelAnimations(tagIndex)
+            if (animationsTag) then
+                local newFpAnimationList = glue.update({}, animationsTag.fpAnimationList)
+                for classValue, permutableValues in pairs(permAnimationClasses) do
+                    dprint(glue.index(animationClasses)[classValue])
+                    newFpAnimationList[classValue] =
+                        math.random(permutableValues[1], permutableValues[#permutableValues])
+                end
+                local animationsTag = blam.modelAnimations(tagIndex)
+                animationsTag.fpAnimationList = newFpAnimationList
+                dprint(animationsTag.fpAnimationList)
             end
         end
-        blam.modelAnimations(get_tag(animationTagId), weaponModelAnimations)
     end
     return true
 end
 
-function onMapLoad()
-    -- Clean permutable animations list
-    permutableAnimations = {}
-    -- Look for tags
-    for tagId = 0, get_tags_count() - 1 do
-        -- Get curren tag type
-        local tagType = get_tag_type(tagId)
-        -- We are looking for model animation tags
-        if (tagType and tagType == tagClasses.modelAnimations) then
-            -- Get current tag path
-            local tagPath = get_tag_path(tagId)
-            -- We are looking for weapon animation tags
-            if (tagPath and tagPath:find('weapon')) then
-                dprint(tagPath)
-                -- Get tag animations data
-                local modelAnim = blam.modelAnimations(get_tag(tagId))
+function mapPermutableAnimations(tagIndex, animationLabel, animationIndex)
+    -- Find permutable animations
+    if (animationLabel:find("%%")) then
+        dprint("Found permutable animation!", "success")
+        dprint(animationLabel)
 
-                -- Iterate through animation list
-                for animationIndex, animation in pairs(modelAnim.animationList) do
-                    local animationLabel = animation.name
-                    -- Find permutable animations
-                    if (animationLabel:find('%%')) then
-                        dprint('Found permutable animation!', 'success')
-                        dprint(animationLabel)
-                        if (not permutableAnimations[tagId]) then
-                            permutableAnimations[tagId] = {}
-                        end
-                        for animationType, animationTypeIndex in
-                            pairs(animationTable) do
-                            if (animationLabel:find(animationType)) then
-                                if (not permutableAnimations[tagId][animationType]) then
-                                    permutableAnimations[tagId][animationType] =
-                                        {}
-                                end
-                                local animationCount =
-                                    #permutableAnimations[tagId][animationType]
-                                dprint(animationCount)
-                                dprint('Adding animation to list.', 'warning')
-                                dprint(animationType, 'category')
-                                permutableAnimations[tagId][animationType][animationCount +
-                                    1] = animationIndex - 1
-                            end
-                        end
+        -- Store space for this animation if doesn't exist
+        if (not availableAnimations[tagIndex]) then
+            availableAnimations[tagIndex] = {}
+        end
+
+        for animationClass, classValue in pairs(animationClasses) do
+            if (animationLabel:find(animationClass)) then
+
+                -- Store space for this animation class if doesn't exist
+                if (not availableAnimations[tagIndex][classValue]) then
+                    availableAnimations[tagIndex][classValue] = {}
+                end
+
+                local currentAnimation = availableAnimations[tagIndex][classValue]
+
+                local animationCount = #currentAnimation
+                dprint("Animation count: " .. animationCount)
+                dprint("Adding animation to list.", "warning")
+                dprint(animationClass, "category")
+                currentAnimation[animationCount + 1] = animationIndex - 1
+            end
+        end
+    end
+
+end
+
+function OnMapLoad()
+    -- Clean permutable animations list
+    OnMapUnload()
+    -- Look for tags
+    for tagIndex = 0, blam.tagDataHeader.count - 1 do
+        -- Get curren tag type
+        local tempTag = blam.getTag(tagIndex)
+        -- We are looking for model animation tags
+        if (tempTag and tempTag.class == tagClasses.modelAnimations) then
+            -- We are looking for weapon animation tags
+            if (tempTag.path and tempTag.path:find("weapon")) then
+                -- Get tag animations data
+                local animationsTag = blam.modelAnimations(tagIndex)
+                if (animationsTag) then
+                    dprint(tempTag.path)
+                    -- Iterate through animation list
+                    for animationIndex, animation in pairs(animationsTag.animationList) do
+                        mapPermutableAnimations(tagIndex, animation.name, animationIndex)
                     end
                 end
             end
@@ -124,13 +144,21 @@ function onMapLoad()
 
     -- Create timer for randomizing animations
     -- This ensures different animations being shown
-    set_timer(250, 'onTimer')
+    keepTimer = true
+    if (not permutatorTimer) then
+        permutatorTimer = set_timer(250, "OnTimer")
+    end
+end
 
+function OnMapUnload()
+    keepTimer = false
+    availableAnimations = {}
 end
 
 -- Register function to event on map laod
-set_callback('map load', 'onMapLoad')
+set_callback("map load", "OnMapLoad")
+set_callback("unload", "OnMapUnload")
 
 -- Run function after loading script
 -- This is to test script changes after reloading script
-onMapLoad()
+-- OnMapLoad()
